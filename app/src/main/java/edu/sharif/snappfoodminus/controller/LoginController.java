b3dd42b7f0593f33fc435ed745cbd358411ee43f
Arya Jalali <nothatcreative5@gmail.com>
2022-07-17 23:56:16 +0430

    sadegh kirame

--- app/src/main/java/edu/sharif/snappfoodminus/controller/LoginController.java
+++ app/src/main/java/edu/sharif/snappfoodminus/controller/LoginController.java
@@ -1,6 +1,40 @@
 package edu.sharif.snappfoodminus.controller;
 
+import android.content.Context;
+
+import java.util.regex.Pattern;
+
 public class LoginController {
 
+    private Context context;
+
+    public LoginController(Context context) {
+        this.context = context;
+    }
+
+    public String getUsernameError(String username) {
+        if (username.isEmpty())
+            return "This field can not be blank";
+        if (username.length() < 3)
+            return "Username must be at least 3 characters long";
+        if (!Pattern.matches("^[a-zA-Z0-9]+$", username))
+            return "Only letters (a-z) and numbers (0-9) are allowed";
+        return null;
+    }
+
+    public String getPasswordError(String password) {
+        if (password.isEmpty())
+            return "This field can not be blank";
+        if (password.length() < 4)
+            return "Password must be at least 4 characters long";
+        return null;
+    }
+
+
+    /* Why kiram to sadegh
+    public User getLoginResult(String username, String password) {
+        return User.getUser(context, username);
+    }
+    */
 
 }
--- app/src/main/java/edu/sharif/snappfoodminus/database/UserDao.java
+++ app/src/main/java/edu/sharif/snappfoodminus/database/UserDao.java
@@ -1,7 +1,34 @@
 package edu.sharif.snappfoodminus.database;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import edu.sharif.snappfoodminus.model.User;
 
 @Dao
 public interface UserDao {
+
+    @Insert
+    void insert(User user);
+
+    @Update
+    public void updateUsers(User... users);
+
+
+    @Query("SELECT * FROM user")
+    List<User> getAllUsers();
+
+    @Query("SELECT * FROM user WHERE username LIKE :username")
+    public User findUserWithUsername(String username);
+
+    @Query("SELECT * FROM user WHERE name LIKE :name")
+    public List<User> findUserWithName(String name);
+
+
+
+
 }
