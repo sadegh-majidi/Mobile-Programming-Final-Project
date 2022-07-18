a7ae36bc55a3a308345bfd009bbb387dc933db0c
Sadegh Majidi <sadegh.majidi@gharar.ir>
2022-07-18 18:41:54 +0430

    Add Restaurant LiveData

--- app/src/main/java/edu/sharif/snappfoodminus/database/RestaurantDao.java
+++ app/src/main/java/edu/sharif/snappfoodminus/database/RestaurantDao.java
@@ -1,7 +1,38 @@
 package edu.sharif.snappfoodminus.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import edu.sharif.snappfoodminus.Constants;
+import edu.sharif.snappfoodminus.model.Restaurant;
 
 @Dao
 public interface RestaurantDao {
+    @Query("SELECT * FROM " + Constants.RESTAURANT_TABLE_NAME + " ORDER BY id ASC")
+    LiveData<List<Restaurant>> getAllRestaurants();
+
+    @Query("SELECT * FROM " + Constants.RESTAURANT_TABLE_NAME + " WHERE owner_id=:ownerId")
+    LiveData<List<Restaurant>> getRestaurantsByOwner(int ownerId);
+
+    @Query("SELECT * FROM " + Constants.RESTAURANT_TABLE_NAME + " WHERE id=:id")
+    LiveData<Restaurant> getRestaurantById(int id);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertRestaurant(Restaurant restaurant);
+
+    @Update
+    void updateRestaurant(Restaurant restaurant);
+
+    @Delete
+    void deleteRestaurant(Restaurant restaurant);
+
+    @Query("DELETE FROM " + Constants.RESTAURANT_TABLE_NAME)
+    void deleteAllRestaurants();
 }
