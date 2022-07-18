4ebf3ed7d4d0c5b011ad4abc818e3c11e6278064
Sadegh Majidi <sadegh.majidi@gharar.ir>
2022-07-18 19:19:37 +0430

    Add Order and OrderItem LiveData

--- app/src/main/java/edu/sharif/snappfoodminus/Constants.java
+++ app/src/main/java/edu/sharif/snappfoodminus/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     public static final String DISCOUNT_TABLE_NAME = "discount";
     public static final String RATING_TABLE_NAME = "rating";
     public static final String REVIEW_TABLE_NAME = "review";
-    public static final String ORDER_TABLE_NAME = "order";
+    public static final String ORDER_TABLE_NAME = "orders";
     public static final String ORDER_ITEM_TABLE_NAME = "order_item";
 }
--- app/src/main/java/edu/sharif/snappfoodminus/database/OrderDao.java
+++ app/src/main/java/edu/sharif/snappfoodminus/database/OrderDao.java
@@ -1,7 +1,41 @@
 package edu.sharif.snappfoodminus.database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import edu.sharif.snappfoodminus.Constants;
+import edu.sharif.snappfoodminus.model.Order;
 
 @Dao
 public interface OrderDao {
+    @Query("SELECT * FROM " + Constants.ORDER_TABLE_NAME + " ORDER BY id ASC")
+    LiveData<List<Order>> getAllOrders();
+
+    @Query("SELECT * FROM " + Constants.ORDER_TABLE_NAME + " WHERE restaurant_id=:restaurantId")
+    LiveData<List<Order>> getOrdersByRestaurant(int restaurantId);
+
+    @Query("SELECT * FROM " + Constants.ORDER_TABLE_NAME + " WHERE user_id=:userId")
+    LiveData<List<Order>> getOrdersByUser(int userId);
+
+    @Query("SELECT * FROM " + Constants.ORDER_TABLE_NAME + " WHERE id=:id")
+    LiveData<Order> getOrderById(long id);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertOrder(Order order);
+
+    @Update
+    void updateOrder(Order order);
+
+    @Delete
+    void deleteOrder(Order order);
+
+    @Query("DELETE FROM " + Constants.ORDER_TABLE_NAME)
+    void deleteAllOrders();
 }
